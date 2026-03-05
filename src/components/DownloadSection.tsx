2d800e1a240b3bede01d54051e73fd3507411d1d
Mahmoud Esmat <mahmoudesmat20029@gmail.com>
2026-03-05 17:48:21 +0200

    some edit on footer

--- src/components/DownloadSection.tsx
+++ src/components/DownloadSection.tsx
@@ -5,11 +5,13 @@ const DownloadSection = () => {
     <section className="back text-brown-foreground h-52 relative">
       <div className="container mx-auto relative h-full">
         {/* Text */}
-        <div className="absolute top-5 sm:top-10 sm:left-20 md:left-32 lg:left-64 text-center">
-          <h2 className="text-2xl md:text-3xl font-black leading-tight mb-2">
+        <div className="flex flex-col items-center justify-center pt-10 md:pt-0 md:absolute top-5 sm:top-10 sm:left-20 md:left-32 lg:left-64 text-center">
+          <h2 className="text-xl sm:text-2xl md:text-3xl font-black leading-tight mb-2">
             حمّل تطبيق مستشارك الهندسي
           </h2>
-          <p className="text-2xl md:text-3xl">وابدأ استشارتك الهندسية بثقة</p>
+          <p className="text-xl sm:text-2xl md:text-3xl">
+            وابدأ استشارتك الهندسية بثقة
+          </p>
 
           <div className="mt-8 flex flex-wrap gap-4 justify-center">
             <button className="bg-foreground/90 text-background font-semibold px-10 py-3 rounded-lg flex items-center gap-2 hover:opacity-90 transition-opacity">
